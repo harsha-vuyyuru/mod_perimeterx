34ee3201e2bc28ef62ed18897602305e6fd683da
omriiluz <omri@iluz.net>
2016-08-12 03:33:37 -0700

    free curl headers

--- src/http_util.c
+++ src/http_util.c
@@ -80,9 +80,11 @@ char *do_request(const char *url, const char *payload, const char *auth_header, 
     curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_response_cb);
     curl_easy_setopt(curl, CURLOPT_WRITEDATA, (void*) &response);
     res = curl_easy_perform(curl);
+    curl_slist_free_all(headers);
     if (res == CURLE_OK) {
         curl_easy_getinfo(curl, CURLINFO_RESPONSE_CODE, &status_code);
         if (status_code == 200) {
+            free(response.data);
             return response.data;
         }
 
@@ -90,6 +92,7 @@ char *do_request(const char *url, const char *payload, const char *auth_header, 
     } else {
         ERROR(r->server, "curl_easy_perform() failed: %s", curl_easy_strerror(res));
     }
+
     free(response.data);
     return NULL;
 }
