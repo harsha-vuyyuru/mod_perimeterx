6b12e1ccfab233b0c079c280e4536448ba152518
Aviad Shikloshi <aviad@perimeterx.com>
2017-07-11 15:00:41 +0300

    update dev to be version 2.4.0-rc.1

--- src/mod_perimeterx.c
+++ src/mod_perimeterx.c
@@ -693,7 +693,7 @@ static void *create_config(apr_pool_t *p) {
         conf->send_page_activities = true;
         conf->blocking_score = 70;
         conf->captcha_enabled = true;
-        conf->module_version = "Apache Module v2.4.0";
+        conf->module_version = "Apache Module v2.4.0-rc.1";
         conf->skip_mod_by_envvar = false;
         conf->curl_pool_size = 40;
         conf->base_url = DEFAULT_BASE_URL;
