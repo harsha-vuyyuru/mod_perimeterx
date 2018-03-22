c9fb05ceb426c1906f344543d33c80a660e6f3ba
Paul Jonkins <paul.ionkin@gmail.com>
2018-03-22 02:43:32 +0100

    Add scripts to build Debian packages

--- build/build.sh
+++ build/build.sh
@@ -4,7 +4,10 @@ if [ ! -f ./build_centos.sh -o ! -f ./build_ubuntu.sh  ]; then
     echo "Please run ./configure script from the project's root directory"
     exit 1
 fi
+/bin/bash ./build_debian.sh
 /bin/bash ./build_centos.sh
 /bin/bash ./build_ubuntu.sh
 
-echo "Packages are in packages/ folder"
+echo
+echo "Packages are in $(pwd)/packages/ folder"
+echo
